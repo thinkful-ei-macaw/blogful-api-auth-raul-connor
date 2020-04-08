463a8fa567e17e2e77eb70f095c867b669a347e9
Connor McCormack <mccormack.connor.m@gmail.com>
2020-04-08 17:20:34 -0500

    all working before checking out new branch

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -11,6 +11,11 @@ describe('Articles Endpoints', function() {
     testComments,
   } = helpers.makeArticlesFixtures()
 
+  function makeAuthHeader(user) {
+    const token = Buffer.from(`${user.user_name}:${user.password}`).toString('base64')
+    return `Basic ${token}`
+  }
+
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
@@ -24,6 +29,62 @@ describe('Articles Endpoints', function() {
   before('cleanup', () => helpers.cleanTables(db))
 
   afterEach('cleanup', () => helpers.cleanTables(db))
+
+  describe(`Protected endpoints`, () => {
+    beforeEach('insert articles', () =>
+      helpers.seedArticlesTables(
+        db,
+        testUsers,
+        testArticles,
+        testComments,
+      )
+    )
+
+    const protectedEndpoints = [
+      {
+        name: 'GET /api/articles/:article_id',
+        path: '/api/articles/1'
+      },
+      {
+        name: 'GET /api/articles/:article_id/comments',
+        path: '/api/articles/1/comments'
+      },
+    ]
+  
+    protectedEndpoints.forEach(endpoint => {
+      describe(endpoint.name, () => {
+        it(`responds with 401 'Missing basic token' when no basic token`, () => {
+          return supertest(app)
+            .get(endpoint.path)
+            .expect(401, { error: `Missing basic token` })
+        })
+
+        it(`responds 401 'Unauthorized request' when no credentials in token`, () => {
+          const userNoCreds = { user_name: '', password: '' }
+          return supertest(app)
+          .get(endpoint.path)
+            .set('Authorization', helpers.makeAuthHeader(userNoCreds))
+            .expect(401, { error: `Unauthorized request` })
+        })
+
+        it(`responds 401 'Unauthorized request' when invalid user`, () => {
+          const userInvalidCreds = { user_name: 'user-not', password: 'existy' }
+          return supertest(app)
+            .get(endpoint.path)
+            .set('Authorization', makeAuthHeader(userInvalidCreds))
+            .expect(401, { error: `Unauthorized request` })
+        })
+
+        it(`responds 401 'Unauthorized request' when invalid password`, () => {
+          const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
+          return supertest(app)
+            .get(endpoint.path)
+            .set('Authorization', makeAuthHeader(userInvalidPass))
+            .expect(401, { error: `Unauthorized request` })
+        })
+      })
+    })
+  })
 
   describe(`GET /api/articles`, () => {
     context(`Given no articles`, () => {
@@ -87,10 +148,14 @@ describe('Articles Endpoints', function() {
 
   describe(`GET /api/articles/:article_id`, () => {
     context(`Given no articles`, () => {
+      beforeEach(() =>
+       db.into('blogful_users').insert(testUsers)
+      )
       it(`responds with 404`, () => {
         const articleId = 123456
         return supertest(app)
           .get(`/api/articles/${articleId}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(404, { error: `Article doesn't exist` })
       })
     })
@@ -115,6 +180,7 @@ describe('Articles Endpoints', function() {
 
         return supertest(app)
           .get(`/api/articles/${articleId}`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(200, expectedArticle)
       })
     })
@@ -137,6 +203,7 @@ describe('Articles Endpoints', function() {
       it('removes XSS attack content', () => {
         return supertest(app)
           .get(`/api/articles/${maliciousArticle.id}`)
+          .set('Authorization', makeAuthHeader(testUser))
           .expect(200)
           .expect(res => {
             expect(res.body.title).to.eql(expectedArticle.title)
@@ -148,10 +215,15 @@ describe('Articles Endpoints', function() {
 
   describe(`GET /api/articles/:article_id/comments`, () => {
     context(`Given no articles`, () => {
+      beforeEach(() =>
+        db.into('blogful_users').insert(testUsers)
+      )
+
       it(`responds with 404`, () => {
         const articleId = 123456
         return supertest(app)
           .get(`/api/articles/${articleId}/comments`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(404, { error: `Article doesn't exist` })
       })
     })
@@ -174,6 +246,7 @@ describe('Articles Endpoints', function() {
 
         return supertest(app)
           .get(`/api/articles/${articleId}/comments`)
+          .set('Authorization', makeAuthHeader(testUsers[0]))
           .expect(200, expectedComments)
       })
     })
--- test/comments-endpoints.spec.js
+++ test/comments-endpoints.spec.js
@@ -33,6 +33,14 @@ describe('Comments Endpoints', function() {
       )
     )
 
+    it(`responds 401 'Unauthorized request' when invalid password`, () => {
+      const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
+      return supertest(app)
+        .post('/api/comments')
+        .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
+        .expect(401, { error: `Unauthorized request` })
+    })
+
     it(`creates an comment, responding with 201 and the new comment`, function() {
       this.retries(3)
       const testArticle = testArticles[0]
@@ -44,6 +52,7 @@ describe('Comments Endpoints', function() {
       }
       return supertest(app)
         .post('/api/comments')
+        .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
         .send(newComment)
         .expect(201)
         .expect(res => {
@@ -89,6 +98,7 @@ describe('Comments Endpoints', function() {
 
         return supertest(app)
           .post('/api/comments')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
           .send(newComment)
           .expect(400, {
             error: `Missing '${field}' in request body`,
